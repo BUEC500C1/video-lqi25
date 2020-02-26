b328357528ecd407f1799ab01d03b1af0e3d2cca
lqi25 <55333782+lqi25@users.noreply.github.com>
2020-02-26 17:52:04 -0500

    Add files via upload

--- Threads/test_queue_mul.py
+++ Threads/test_queue_mul.py
@@ -1,16 +1,19 @@
 import pytest
 from Twitter2Video import image2video,tweet2image
 from queue_mul import Mul_Threads
+import os
 def test_result():
     item_list = ['BU_Tweets','CNN','Nike','mfaboston']
-    item_list_2 = ['BU_Tweets', 'CNN', 'Nike', 'mfaboston', 'BU_ece', 'BostonDynamics', 'realDonaldTrump', 'WHO', 'TIME']
     assert Mul_Threads([],2) == "No twitter names entered"
     assert Mul_Threads(item_list,0) == "The num should be bigger than 0"
     # No twitter keys, can't test.
     '''
-    assert Mul_Threads(item_list,3) == "All threads have completed!"
-    assert Mul_Threads(item_list, 4) == "All threads have completed!"
-    assert Mul_Threads(item_list, 5) == "All threads have completed!"
-    assert Mul_Threads(item_list_2, 6) == "All threads have completed!"
-    assert Mul_Threads(item_list_2, 10) == "All threads have completed!"
+    Mul_Threads(item_list, 4)
+    assert os.path.exists('BU_Tweets.avi') == True
+    assert os.path.exists('CNN.avi') == True
+    assert os.path.exists('Nike.avi') == True
+    assert os.path.exists('mfaboston.avi') == True
+    assert os.path.exists('celtics.avi') == False
     '''
+
+#test_result()
